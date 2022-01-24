1092913c729e1ad780ba647c2ae54cad54fd4e52
Paige <pagewortmann@gmail.com>
2022-01-24 15:13:30 -0500

    Testing progress

--- Test/PermutationsTest.java
+++ Test/PermutationsTest.java
@@ -1,22 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class PermutationsTest {
 
     @Test
     public void null_word_returns_validation() {
-        // Arrange
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        // Act
-        System.err.println("Please enter a valid value");
-
-        // Assert
-        assertEquals("Please enter a valid value", out.toString());
+        Permutations sut = new Permutations();
+        sut.toString();
 
     }
 }
